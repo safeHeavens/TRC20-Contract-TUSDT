d38c79704ff45e914113733c81419cc4680c0897
Samson Belario <belariosammy@gmail.com>
2024-08-16 20:37:20 -0700

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,6 +1,6 @@
 // 0.5.1-c8a2
 // Enable optimization
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.0;
 
 import "./TRC20.sol";
 import "./TRC20Detailed.sol";
@@ -16,7 +16,7 @@ contract Token is TRC20, TRC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public TRC20Detailed("YourTokenName", "YTN", 18) {
+    constructor () public TRC20Detailed("Tether USD", "USDT", 6) {
         _mint(msg.sender, 10000000000 * (10 ** uint256(decimals())));
     }
 }
